bc43c5309b8049b7594fad0dd3b698371986ed28
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2021-09-08 16:23:09 +0530

    Create pv1.js

---
+++ pv1.js
@@ -0,0 +1,11 @@
+console.clear(), console.log("%c%s", "font-size:large; font-weight:bold",document.querySelector("td.unselectable>span.Mytd.unselectable").innerText + "\n\n" + document.querySelector("#response_text_display").innerText), console.log("%c%s", "font-size:large; font-weight:bold",document.querySelector("td.unselectable>span.Mytd.unselectable").innerText.replace(/[^\u0000-\u007F]/g, "") + "\n\n" + document.querySelector("#response_text_display").innerText.replace(/[^\u0000-\u007F]/g, "")), document.getElementById("previous").addEventListener("click", function () {
+    setTimeout(function () {
+        console.clear(), console.log("%c%s", "font-size:large; font-weight:bold",document.querySelector("td.unselectable>span.Mytd.unselectable").innerText + "\n\n" + document.querySelector("#response_text_display").innerText), console.log("%c%s", "font-size:large; font-weight:bold",document.querySelector("td.unselectable>span.Mytd.unselectable").innerText.replace(/[^\u0000-\u007F]/g, "") + "\n\n" + document.querySelector("#response_text_display").innerText.replace(/[^\u0000-\u007F]/g, ""))
+    }, 1e3)
+}), document.getElementById("next").addEventListener("click", function () {
+    setTimeout(function () {
+        console.clear(), console.log("%c%s", "font-size:large; font-weight:bold",document.querySelector("td.unselectable>span.Mytd.unselectable").innerText + "\n\n" + document.querySelector("#response_text_display").innerText), console.log("%c%s", "font-size:large; font-weight:bold",document.querySelector("td.unselectable>span.Mytd.unselectable").innerText.replace(/[^\u0000-\u007F]/g, "") + "\n\n" + document.querySelector("#response_text_display").innerText.replace(/[^\u0000-\u007F]/g, ""))
+    }, 1e3)
+}), document.body.style.zoom = "80%";
+
+//chrome://inspect/#devices
